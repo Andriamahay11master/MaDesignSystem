0de64a0f12fedbaec29eeff2b0b6a8f1d1e6af23
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-24 16:45:22 +0300

    ok config route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Color from './page/color/Color'
 
-function App() {
 
+function App() {
   return (
-    <>
-      <Color/>
-    </>
-  )
+    <Router>
+        <Routes>
+          <Route path='/color' element={<Color/>}/>
+        </Routes>
+    </Router>
+  );
 }
 
 export default App
