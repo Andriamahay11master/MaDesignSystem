2382c6b4d483691c13f2903bfd974a915f30eb31
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-26 17:37:49 +0300

    init page color

--- src/page/color/Color.tsx
+++ src/page/color/Color.tsx
@@ -2,11 +2,33 @@ import React from 'react'
 import './color.scss'
 
 export default function Color() {
+    const dataPalette = [
+        {
+            bg: 'Primary',
+            name: 'Gray 500',
+            text: '#838E9E'
+        }
+    ]
     return (
         <div className='page'>
-            <div className="section">
+            <div className="section section-color">
                 <div className="container">
-                    <h1>Colors</h1>
+                    <div className="color-content-top">
+                        <h1>Colors</h1>
+                        <p>Color is used to define the main color of the design system. It is used in the header, footer and other areas of the site.</p>    
+                    </div>
+                    <div className="color-content-bottom">
+                        <h2>Color Palette</h2>
+                        <div className="color-palette">
+                            {dataPalette.map((item, index) => (
+                                <div key={index} className="color-box">
+                                    <div className="color-box__bg" style={{backgroundColor: item.text}}></div>
+                                    <span className="color-box__name">{item.name}</span>
+                                    <span className="color-box__text">{item.text}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
