c01a6bed649fca00ac09f6dcf917333deb604dbd
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-30 14:07:17 +0300

    add variant 3

---
+++ src/components/alert/Alert-2.tsx
@@ -0,0 +1,24 @@
+import './alert.scss'
+
+interface Alert2Props{
+    type: string
+    title: string
+    ctn: string
+}
+export default function Alert2({type, title, ctn} : Alert2Props){ 
+    return (
+        <div className={`alert alert-2 alert-${type}`} >
+            <div className="alert-col">
+                <div className="alert-icon">
+                    <i className={`icon icon-${type}`}></i>
+                </div>
+                <h3 className="title-h3">{title}</h3>
+            </div>
+            <div className="alert-col">
+                <div className="alert-bouton">
+                    {ctn && <button className={`btn btn-${type}`}>{ctn}</button>}
+                </div>  
+            </div>
+        </div>
+    );
+}
--- src/page/alert/PageAlert.tsx
+++ src/page/alert/PageAlert.tsx
@@ -1,4 +1,5 @@
 import Alert from "../../components/alert/Alert";
+import Alert2 from "../../components/alert/Alert-2";
 import Header from "../../components/header/Header";
 import Top from "../../components/top/Top";
 import { dataNav } from "../../data/menu";
@@ -20,6 +21,10 @@ export default function PageAlert(){
                         <Alert type='sucess' title="This is a success alert" content="Success is a UI component that provides immediate, visually distinct feedback or information to users, categorized by type such as success, error, warning, or info." ctn="View Profile" close="Close"/>
                         <Alert type="warning" title="This is a warning alert" content="Warning is a UI component that provides immediate, visually distinct feedback or information to users, categorized by type such as success, error, warning, or info." ctn="Forget Password" close="Close"/>
                         <Alert type="error" title="This is a error alert" content="Error is a UI component that provides immediate, visually distinct feedback or information to users, categorized by type such as success, error, warning, or info." ctn="Upgrade Plans" close="Close"/>
+                        <Alert2 type='info' title='This is a info alert' ctn="Check Security" />
+                        <Alert2 type='sucess' title="This is a success alert" ctn="View Profile" />
+                        <Alert2 type="warning" title="This is a warning alert" ctn="Forget Password"/>
+                        <Alert2 type="error" title="This is a error alert" ctn="Upgrade Plans"/>
                     </div>
                 </div>
             </div>
