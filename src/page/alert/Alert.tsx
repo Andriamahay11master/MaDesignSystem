6fb637111daf049aefbc8dca0465bd9c4ef0926c
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-30 09:52:29 +0300

    init page alert

---
+++ src/page/alert/Alert.tsx
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import Top from "../../components/top/Top";
+import { dataNav } from "../../data/menu";
+
+export default function Alert(){
+    return (
+        <div className='page'>
+            <Header linkMenu={dataNav}/>
+            <div className="section section-alert">
+                <div className="container">
+                    <Top title='Component/Alert' content='Alert is a UI component that provides immediate, visually distinct feedback or information to users, categorized by type such as success, error, warning, or info.'/>
+                </div>
+            </div>
+        </div>    
+    )
+}
