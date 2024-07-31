e01cc67223eea3371b872008083b2fb471a9f4fb
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-31 11:57:53 +0300

    remove unsed code

--- src/page/color/Color.tsx
+++ src/page/color/Color.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './color.scss'
 import { DataPaletteType } from '../../models/DataPaletteType';
 import { colors } from '../../data/color';
